5a7429261f34c5ad093f02feb748cee42a8c9956
kmg159753 <mingyugim584@gmail.com>
2023-06-08 13:23:31 +0900

    Update main.js

--- static/main.js
+++ static/main.js
@@ -68,152 +68,152 @@ function show_info() {
     let star_repeat = '⭐'.repeat(star) 
 
     let tmep_html = `<div class="container-wrap">
-    <div class="information-wrap">
-        <div class="information">기본정보</div>
-    </div>
-    <div class="name-contact-email">
-        <div class="information-wrap-namebox">
-            <div class="name">이름</div>
-            <input class="namebox" type="text" placeholder="이름" id="name"/>
-        </div>
-        <div class="information-wrap-contactbox">
-            <div class="contact">연락처</div>
-            <input class="contactbox" type="text" placeholder="연락처" id="contacts"/>
-        </div>
-        <div class="information-wrap-emailbox">
-            <div class="email">이메일</div>
-            <input class="emailbox" type="text" placeholder="메일" id="email"/>
-        </div>
-    </div>
-</div>
-<!-- 선 -->
-<div class="line-divde"></div>
-<!-- 희망 직무 & 직무 경력 -->
-    <div class="container-wrap1">
-      <div class="type-of-job">
-        <div class="job-select">희망 직무</div>
-        <div class="job-select1">
-            <div class="information-wrap-namebox">
-                <div>
-                    <label class="small-job-select-title" for="inputGroupSelect01">직군</label>
-                  </div>
-    
-                <select class="small-job-select-option" id="desired_position">
-                    <option selected>-- 선택하기 --</option>
-                    <option value="1">개발</option>
-                    <option value="2">기획</option>
-                    <option value="3">마케팅</option>
-                    <option value="4">디자인</option>
-                    <option value="5">경영,인사,운영</option>
-                    <option value="6">기타</option>
-                    
-                  </select>
-            </div>
-            <div class="information-wrap-namebox">
-                <div>
-                    <label class="small-job-select-title" for="inputGroupSelect01">직무</label>
-                </div>
-    
-                <select class="small-job-select-option" id="desired_role">
-                    <option selected>-- 선택하기 --</option>
-                    <option value="1">프론트엔드 개발자</option>
-                    <option value="2">서버/백앤드 개발자</option>
-                    <option value="3">웹 풀스택 개발자</option>
-                    <option value="4">앱 개발자</option>
-                    <option value="5">머신러닝/인공지능 개발자</option>
-                    <option value="6">게임 개발자</option>
-                    <option value="7">기타</option>
-                    
-                  </select>
-            </div>
-            <div class="information-wrap-namebox">
-                <div>
-                    <label class="small-job-select-title" for="inputGroupSelect01">경력</label>
-                </div>
-    
-                <select class="small-job-select-option" id="desired_work_exp">
-                    <option selected>-- 선택하기 --</option>
-                    <option value="1">직무 관련 경력 없음</option>
-                    <option value="2">1년 차</option>
-                    <option value="3">2년 차</option>
-                    <option value="4">3년 차</option>
-                    <option value="5">4년 차</option>
-                    <option value="6">5년 차</option>
-                    <option value="7">6년 차</option>
-                    <option value="8">7년 차 이상</option>
-                  </select>
-            </div>  
+                        <div class="information-wrap">
+                            <div class="information">기본정보</div>
+                        </div>
+                        <div class="name-contact-email">
+                            <div class="information-wrap-namebox">
+                                <div class="name">이름</div>
+                                <input class="namebox" type="text" placeholder="이름" id="name"/>
+                            </div>
+                            <div class="information-wrap-contactbox">
+                                <div class="contact">연락처</div>
+                                <input class="contactbox" type="text" placeholder="연락처" id="contacts"/>
+                            </div>
+                            <div class="information-wrap-emailbox">
+                                <div class="email">이메일</div>
+                                <input class="emailbox" type="text" placeholder="메일" id="email"/>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- 선 -->
+                    <div class="line-divde"></div>
+                    <!-- 희망 직무 & 직무 경력 -->
+                        <div class="container-wrap1">
+                          <div class="type-of-job">
+                            <div class="job-select">희망 직무</div>
+                            <div class="job-select1">
+                                <div class="information-wrap-namebox">
+                                    <div>
+                                        <label class="small-job-select-title" for="inputGroupSelect01">직군</label>
+                                      </div>
+                        
+                                    <select class="small-job-select-option" id="desired_position">
+                                        <option selected>-- 선택하기 --</option>
+                                        <option value="1">개발</option>
+                                        <option value="2">기획</option>
+                                        <option value="3">마케팅</option>
+                                        <option value="4">디자인</option>
+                                        <option value="5">경영,인사,운영</option>
+                                        <option value="6">기타</option>
+                                        
+                                      </select>
+                                </div>
+                                <div class="information-wrap-namebox">
+                                    <div>
+                                        <label class="small-job-select-title" for="inputGroupSelect01">직무</label>
+                                    </div>
+                        
+                                    <select class="small-job-select-option" id="desired_role">
+                                        <option selected>-- 선택하기 --</option>
+                                        <option value="1">프론트엔드 개발자</option>
+                                        <option value="2">서버/백앤드 개발자</option>
+                                        <option value="3">웹 풀스택 개발자</option>
+                                        <option value="4">앱 개발자</option>
+                                        <option value="5">머신러닝/인공지능 개발자</option>
+                                        <option value="6">게임 개발자</option>
+                                        <option value="7">기타</option>
+                                        
+                                      </select>
+                                </div>
+                                <div class="information-wrap-namebox">
+                                    <div>
+                                        <label class="small-job-select-title" for="inputGroupSelect01">경력</label>
+                                    </div>
+                        
+                                    <select class="small-job-select-option" id="desired_work_exp">
+                                        <option selected>-- 선택하기 --</option>
+                                        <option value="1">직무 관련 경력 없음</option>
+                                        <option value="2">1년 차</option>
+                                        <option value="3">2년 차</option>
+                                        <option value="4">3년 차</option>
+                                        <option value="5">4년 차</option>
+                                        <option value="6">5년 차</option>
+                                        <option value="7">6년 차</option>
+                                        <option value="8">7년 차 이상</option>
+                                      </select>
+                                </div>  
 
-            <div class="information-wrap-emailbox">
-              <div class="email">직무 스킬</div>
-              <input class="emailbox" type="text" placeholder="스킬" id="desired_maj_skill"/>
-          </div>
-          
-        </div>
-      </div>
-              
-    </div>
-<div class="line-divde"></div>
+                                <div class="information-wrap-emailbox">
+                                  <div class="email">직무 스킬</div>
+                                  <input class="emailbox" type="text" placeholder="스킬" id="desired_maj_skill"/>
+                              </div>
+                              
+                            </div>
+                          </div>
+                                  
+                        </div>
+                    <div class="line-divde"></div>
 
-<!-- 최종 경력 -->
-<div class="container-wrap">
-    <div class="career">
-        <div class="final-career">최종 경력</div>
-        <div class="final-career-under"></div>
-      </div>
-      <div class="choose-career">
-        <div class="choose-career">              
-          <div class="information-wrap-namebox">
-        
-            <div class="name" style="font-size: 15px;">경력 구분</div>
-            <div class="career-division-select">
-              <!-- 버튼 누른값 가져오기 검색하면 가져올수 있습니당. -->
-              <button type="button" class="btn btn-outline-primary" style="width: 200px;">경력</button>
-              <button type="button" class="btn btn-outline-primary" style="width: 200px;">신입</button>
-            </div>
-        </div>
-        </div>
-      </div>
-</div>
+                    <!-- 최종 경력 -->
+                    <div class="container-wrap">
+                        <div class="career">
+                            <div class="final-career">최종 경력</div>
+                            <div class="final-career-under"></div>
+                          </div>
+                          <div class="choose-career">
+                            <div class="choose-career">              
+                              <div class="information-wrap-namebox">
+                            
+                                <div class="name" style="font-size: 15px;">경력 구분</div>
+                                <div class="career-division-select">
+                                  <!-- 버튼 누른값 가져오기 검색하면 가져올수 있습니당. -->
+                                  <button type="button" class="btn btn-outline-primary" style="width: 200px;">경력</button>
+                                  <button type="button" class="btn btn-outline-primary" style="width: 200px;">신입</button>
+                                </div>
+                            </div>
+                            </div>
+                          </div>
+                    </div>
 
-<div class="line-divde"></div>
-  <div class="education">
-    
-    <div class="education-title-wrap">
-      <div class="education-title">최종 학력</div>
-      <div class="education-under"></div>
-    </div>
-    <div class="school-name-wrap">
-        <div class="name-contact-email">
-            <div class="information-wrap-namebox">
-                <div class="name">학교명</div>
-                <input class="namebox" type="text" placeholder="학교명 입력" id="last_company_name"/>
-            </div>
-            <div class="information-wrap-namebox">
-                <label class="complete-title" for="inputGroupSelect01">이수 상태</label>
-    
-                <select class="small-job-select-option" id="last_comp_stat">
-                    <option selected>-- 선택하기 --</option>
-                    <option value="1">졸업</option>
-                    <option value="2">재학중</option>
-                    <option value="3">휴학중</option>
-                    <option value="4">수료</option>
-                    <option value="5">중퇴</option>
-                    <option value="6">자퇴</option>
-                    <option value="7">졸업예정</option>
-                  </select>
-            </div>
-            <div class="complete">
-                
-              </div>   
-            <div class="information-wrap-emailbox">
-                <div class="email">학과명</div>
-                <input class="emailbox" type="text" placeholder="학과명 입력" id="last_major"/>
-            </div>
-        </div>         
-             
-    </div>
-  </div> ` 
+                    <div class="line-divde"></div>
+                      <div class="education">
+                        
+                        <div class="education-title-wrap">
+                          <div class="education-title">최종 학력</div>
+                          <div class="education-under"></div>
+                        </div>
+                        <div class="school-name-wrap">
+                            <div class="name-contact-email">
+                                <div class="information-wrap-namebox">
+                                    <div class="name">학교명</div>
+                                    <input class="namebox" type="text" placeholder="학교명 입력" id="last_company_name"/>
+                                </div>
+                                <div class="information-wrap-namebox">
+                                    <label class="complete-title" for="inputGroupSelect01">이수 상태</label>
+                        
+                                    <select class="small-job-select-option" id="last_comp_stat">
+                                        <option selected>-- 선택하기 --</option>
+                                        <option value="1">졸업</option>
+                                        <option value="2">재학중</option>
+                                        <option value="3">휴학중</option>
+                                        <option value="4">수료</option>
+                                        <option value="5">중퇴</option>
+                                        <option value="6">자퇴</option>
+                                        <option value="7">졸업예정</option>
+                                      </select>
+                                </div>
+                                <div class="complete">
+                                    
+                                  </div>   
+                                <div class="information-wrap-emailbox">
+                                    <div class="email">학과명</div>
+                                    <input class="emailbox" type="text" placeholder="학과명 입력" id="last_major"/>
+                                </div>
+                            </div>         
+                                
+                        </div>
+                      </div> ` 
 
     $('#con').append(tmep_html)
 
